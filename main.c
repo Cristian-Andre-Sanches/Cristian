b740f09463b55315e954d81cc99c9071084906f7
Cristian Andre Sanches <54078919+Cristian-Andre-Sanches@users.noreply.github.com>
2019-08-27 16:46:40 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,28 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
-void Conversao(int *horas,int *minutos,int TotalMinutos)
-{
-    if (TotalMinutos>=60)
-    {
-        *horas=TotalMinutos/60;
-        *minutos=TotalMinutos-(*horas * 60);
+#include <stdbool.h>
+typedef struct {
+    int chave;
+} Objeto;
+
+typedef struct NoPilha *PtrNoPilha;
+
+typedef struct NoPilha{
+  Objeto obj;
+  PtrNoPilha proximo;
+} NoPilha;
+
+typedef struct {
+  PtrNoPilha topo;
+  int tamanho;
+} PilhaDinamica;
+
+
+void iniciaPilha(PilhaDinamica *p){
+    p->topo=NULL;
+    p->tamanho=0;
+}
+
+int tamanho(PilhaDinamica*p){
+    return (p->tamanho);
+}
+
+bool estaVazia(PilhaDinamica*p){
+    //if(p->tamhanho==0){
+      //  return (true)
+    //}else { return (false)
+    return (p->tamanho==0);
     }
-    else
-    {
-        *minutos=TotalMinutos;
+
+void empilha(Objeto obj, PilhaDinamica *p){
+
+    Objeto pop(PilhaDinamica *p){
+        if(estaVazia(p)){
+            PtrNoPilha Aux;
+            Aux =topo(p);
+            topo = topo -> proximo;
+        }
     }
 }
+
 int main()
 {
-    int TotalMinutos, horas, minutos;
-    printf("Digite o total de minutos:\n");
-    scanf("%d",&TotalMinutos);
 
-    horas=0;
-    minutos=0;
-
-    Conversao(&horas,&minutos,TotalMinutos);
-    printf("Horas %i:%i",horas,minutos);
     return 0;
 }
-
